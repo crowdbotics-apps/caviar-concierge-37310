03bedfeafd6822e24ddc47f562230ca17e64188a
Crowdbotics <team@crowdbotics.com>
2022-10-14 21:06:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "caviar_concierge_37310",
+  url: "https://caviar_concierge_37310.botics.co",
+  api: "https://caviar_concierge_37310.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
